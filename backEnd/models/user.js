9e1769148befe53253e5e4c9671f3578d454b014
Tibo <thibault.astier@ens.uvsq.fr>
2026-03-30 17:37:22 +0200

    création de la base de données

---
+++ backEnd/models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const argon2 = require("argon2");
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    wins: { type: Number, default: 0 },
+    losses: { type: Number, default: 0 },
+    ratio: { type: Number, default: 0 },
+});
+
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await argon2.hash(this.password);
+    }
+    next();
+});
+
+userSchema.methods.verifyPassword = async function (password) {
+    return await argon2.verify(this.password, password);
+};
+
+module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const fs = require("fs");
 const app = express();
+const mongoose = require("mongoose");
 
 const PORT = 3000;
 const https = require("https");
@@ -10,6 +11,13 @@ const privateKey = fs.readFileSync("key.pem");
 const certificate = fs.readFileSync("cert.pem");
 
 const credentials = { key: privateKey, cert: certificate };
+const uri = "mongodb://thibault971:kyrCo4-gibseg-nebsoh@ac-ggrzodb-shard-00-00.gpzde5z.mongodb.net:27017,ac-ggrzodb-shard-00-01.gpzde5z.mongodb.net:27017,ac-ggrzodb-shard-00-02.gpzde5z.mongodb.net:27017/?ssl=true&replicaSet=atlas-juz2qs-shard-0&authSource=admin&appName=AWS";
+mongoose.connect(uri)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    }).catch((err) => {
+        console.log(err);
+    });
 
 app.use(express.static(path.join(__dirname, "frontEnd")));
 
